5ed558fded66e38ccd4ec090903365896c72b247
DESKTOP-L65KPV5\steng <stengelnestordario28@gmail.com>
2021-04-10 11:06:31 +0200

    Project Finished

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+    <Main/>
+   
+    <Card/>
+    </div>
+  );
+}
+
+function Main(){
+  return(
+  <div className="jumbotron">
+    <h1>A Warm Welcome</h1>
+    <p>Et eu do consectetur consectetur deserunt occaecat nulla consectetur. Nulla exercitation commodo occaecat ad officia veniam reprehenderit deserunt non ad sunt enim est incididunt. Et est deserunt laborum magna commodo sunt aliquip proident ex elit. Magna do qui adipisicing consequat officia laborum cupidatat deserunt commodo non qui. Consequat id eu ut elit duis duis voluptate minim deserunt commodo. Et magna enim enim ea.</p>
+
+    <button type="button" className="btn btn-primary">Call To Action</button>
+  </div>
+  );
+}
+
+function Card(){
+  return(
+    <div className="card-deck">
+      <div className="card">
+       <div className="card-body text-center">
+      <p className="card-text">Dolore non in anim ad ad reprehenderit adipisicing nulla duis. Sunt eu occaecat laborum in occaecat incididunt. Adipisicing excepteur Lorem veniam anim aute aute cillum ullamco nostrud nostrud mollit culpa laboris. Incididunt eu in duis laboris id ea. Deserunt mollit dolore occaecat laboris laborum sit proident do sunt laborum laboris veniam fugiat. Et esse ex reprehenderit quis proident duis consequat esse enim nostrud aliqua qui. Veniam quis mollit proident irure amet dolor quis enim exercitation reprehenderit irure.Aute id ut culpa enim in sunt consequat ut elit ea. Adipisicing do veniam nulla cillum. Cillum consectetur veniam sint veniam officia tempor labore ullamco reprehenderit dolor et. Velit ea Lorem cupidatat reprehenderit deserunt et quis velit consectetur culpa.</p>
+      <button type="button" className="btn btn-primary">Find Out More</button>
+    </div>
+      </div>
+      <div className="card">
+       <div className="card-body text-center">
+      <p className="card-text">Dolore non in anim ad ad reprehenderit adipisicing nulla duis. Sunt eu occaecat laborum in occaecat incididunt. Adipisicing excepteur Lorem veniam anim aute aute cillum ullamco nostrud nostrud mollit culpa laboris. Incididunt eu in duis laboris id ea. Deserunt mollit dolore occaecat laboris laborum sit proident do sunt laborum laboris veniam fugiat. Et esse ex reprehenderit quis proident duis consequat esse enim nostrud aliqua qui. Veniam quis mollit proident irure amet dolor quis enim exercitation reprehenderit irure.Aute id ut culpa enim in sunt consequat ut elit ea. Adipisicing do veniam nulla cillum. Cillum consectetur veniam sint veniam officia tempor labore ullamco reprehenderit dolor et. Velit ea Lorem cupidatat reprehenderit deserunt et quis velit consectetur culpa.</p>
+      <button type="button" className="btn btn-primary">Find Out More</button>
+    </div>
+      </div>
+      <div className="card">
+       <div className="card-body text-center">
+      <p className="card-text">Dolore non in anim ad ad reprehenderit adipisicing nulla duis. Sunt eu occaecat laborum in occaecat incididunt. Adipisicing excepteur Lorem veniam anim aute aute cillum ullamco nostrud nostrud mollit culpa laboris. Incididunt eu in duis laboris id ea. Deserunt mollit dolore occaecat laboris laborum sit proident do sunt laborum laboris veniam fugiat. Et esse ex reprehenderit quis proident duis consequat esse enim nostrud aliqua qui. Veniam quis mollit proident irure amet dolor quis enim exercitation reprehenderit irure.Aute id ut culpa enim in sunt consequat ut elit ea. Adipisicing do veniam nulla cillum. Cillum consectetur veniam sint veniam officia tempor labore ullamco reprehenderit dolor et. Velit ea Lorem cupidatat reprehenderit deserunt et quis velit consectetur culpa.</p>
+      <button type="button" className="btn btn-primary">Find Out More</button>
+    </div>
+      </div>
+      <div className="card">
+       <div className="card-body text-center">
+      <p className="card-text">Dolore non in anim ad ad reprehenderit adipisicing nulla duis. Sunt eu occaecat laborum in occaecat incididunt. Adipisicing excepteur Lorem veniam anim aute aute cillum ullamco nostrud nostrud mollit culpa laboris. Incididunt eu in duis laboris id ea. Deserunt mollit dolore occaecat laboris laborum sit proident do sunt laborum laboris veniam fugiat. Et esse ex reprehenderit quis proident duis consequat esse enim nostrud aliqua qui. Veniam quis mollit proident irure amet dolor quis enim exercitation reprehenderit irure.Aute id ut culpa enim in sunt consequat ut elit ea. Adipisicing do veniam nulla cillum. Cillum consectetur veniam sint veniam officia tempor labore ullamco reprehenderit dolor et. Velit ea Lorem cupidatat reprehenderit deserunt et quis velit consectetur culpa.</p>
+      <button type="button" className="btn btn-primary">Find Out More</button>
+    </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
